5c26826c2977321ba9494d18179db1d1aa3d6191
Warda Nadir <2021bbit83@student.uet.edu.pk>
2024-03-20 09:36:13 +0500

    main

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    var questions = document.querySelectorAll('.question');
+    
+    questions.forEach(function (question) {
+        question.addEventListener('click', function () {
+            
+            this.classList.toggle('active');
+            var answer = this.nextElementSibling;
+            if (answer.style.display === 'block') {
+                answer.style.display = 'none';
+            } else {
+                answer.style.display = 'block';
+            }
+        });
+    });
+});
+
+
+
+document.getElementById('payment-form').addEventListener('submit', function(event) {
+    
+    event.preventDefault();
+  
+   
+    alert('Congratulations! Your payement details are confirmed');
+});
+
+
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    
+    event.preventDefault();
+  
+   
+    alert('Welcome to BRANSTORE!!');
+});
+
+
+  
